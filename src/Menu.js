1aba89099482a6a7f384d1fe0a3770e1083bf62f
Viktor <vityaschel@utidteam.com>
2021-12-09 14:01:15 +0400

    Cards, localization markup

--- src/Menu.js
+++ src/Menu.js
@@ -1,3 +1,6 @@
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
 export default function Menu() {
   return (
     <MenuItem id='me' />
@@ -5,5 +8,5 @@ export default function Menu() {
 }
 
 function MenuItem(props) {
-  
+  const shape = useLoader(GLTFLoader, `/models/cards/`)
 }
