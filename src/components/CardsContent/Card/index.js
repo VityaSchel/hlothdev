3a970f5b6e16d171f7fbfdb9313516fc1ad06a41
Viktor <vityaschel@utidteam.com>
2021-12-19 19:59:17 +0400

    Links on cards

--- src/components/CardsContent/Card/index.js
+++ src/components/CardsContent/Card/index.js
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types'
 import styles from './styles.module.scss'
 import { connect } from 'react-redux'
 import cx from 'classnames'
+import { MdChevronRight } from 'react-icons/md'
 
 import Typography from '@mui/material/Typography'
+import ButtonBase from '@mui/material/ButtonBase'
 
 Card.propTypes = {
   children: PropTypes.node,
@@ -12,33 +14,39 @@ Card.propTypes = {
   subtitle: PropTypes.string,
   caption: PropTypes.string,
   theme: PropTypes.string,
+  link: PropTypes.string,
 }
 
 function Card(props) {
   return (
-    <div className={cx(styles.card, { [styles.darkTheme]: props.theme === 'dark' })}>
-      <div className={styles.avatar}>
-        {props.avatar}
-      </div>
-      <div className={styles.content}>
-        <div className={styles.title}>
-          <Typography variant='h6' gutterBottom component='div'>
-            {props.title}
-          </Typography>
+    <ButtonBase className={styles.container}>
+      <a className={cx(styles.card, { [styles.darkTheme]: props.theme === 'dark' })} href={props.link}>
+        <div className={styles.avatar}>
+          {props.avatar}
         </div>
-        <div className={styles.subtitle}>
-          <Typography variant='subtitle1' gutterBottom component='div'>
-            {props.subtitle}
-          </Typography>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <Typography variant='h6' gutterBottom component='div'>
+              {props.title}
+            </Typography>
+          </div>
+          <div className={styles.subtitle}>
+            <Typography variant='subtitle1' gutterBottom component='div'>
+              {props.subtitle}
+            </Typography>
+          </div>
+          {props.children}
+          <div className={styles.caption}>
+            <Typography variant='overline' gutterBottom display='block'>
+              {props.caption}
+            </Typography>
+          </div>
         </div>
-        {props.children}
-        <div className={styles.caption}>
-          <Typography variant='overline' gutterBottom display='block'>
-            {props.caption}
-          </Typography>
+        <div className={styles.chevron}>
+          <MdChevronRight />
         </div>
-      </div>
-    </div>
+      </a>
+    </ButtonBase>
   )
 }
 
--- src/components/CardsContent/services.js
+++ src/components/CardsContent/services.js
@@ -1,28 +1,52 @@
+import PropTypes from 'prop-types'
 import Card from './Card'
 import LanguageIcon from '@mui/icons-material/Language'
+import { MdOutlineSpaceDashboard } from 'react-icons/md'
+import { GiRobotAntennas, GiAbstract016 } from 'react-icons/gi'
 import styles from './styles.module.scss'
+import mailTo from 'mailto-link'
+import { connect } from 'react-redux'
 
-export default function Services() {
+Services.propTypes = {
+  translation: PropTypes.object,
+}
+
+function Services(props) {
+  const cards = props.translation.SERVICES_CARDS
+  if(!cards) return <></>
+
   return (
     <div className={styles.services}>
       <Card
-        avatar={<LanguageIcon />}
-        title='Разработка лендинга'
-        subtitle=''
-        caption=''
+        avatar={<MdOutlineSpaceDashboard />}
+        title={cards.landing.title}
+        subtitle={cards.landing.description}
+        caption={cards.landing.price}
+        link={mailTo({ to: 'hi@hloth.dev', subject: cards.landing.title })}
       />
       <Card
         avatar={<LanguageIcon />}
-        title='Разработка сайта «под ключ»'
-        subtitle=''
-        caption=''
+        title={cards.turnkey_website.title}
+        subtitle={cards.turnkey_website.description}
+        caption={cards.turnkey_website.price}
+        link={mailTo({ to: 'hi@hloth.dev', subject: cards.turnkey_website.title })}
       />
       <Card
-        avatar={<LanguageIcon />}
-        title='Разработка бота'
-        subtitle=''
-        caption=''
+        avatar={<GiRobotAntennas />}
+        title={cards.bot.title}
+        subtitle={cards.bot.description}
+        caption={cards.bot.price}
+        link={mailTo({ to: 'hi@hloth.dev', subject: cards.bot.title })}
+      />
+      <Card
+        avatar={<GiAbstract016 />}
+        title={cards.other.title}
+        subtitle={cards.other.description}
+        caption={cards.other.price}
+        link={mailTo({ to: 'hi@hloth.dev', subject: cards.other.title })}
       />
     </div>
   )
 }
+
+export default connect(state => ({ translation: state.translation }))(Services)
