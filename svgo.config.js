57cc8a0c831ab01f6b9c002635ea14e51997655c
Viktor <vityaschel@utidteam.com>
2022-03-07 16:39:40 +0400

    Cleaning svg

--- svgo.config.js
+++ svgo.config.js
@@ -7,6 +7,39 @@ module.exports = {
       }
     },
     'removeComments',
-    'cleanupIDs'
+    {
+      name: 'cleanupIDs',
+      params: {
+        prefix: {
+          toString() {
+            this.counter = this.counter || 0
+
+            return `id-${this.counter++}`
+          }
+        }
+      }
+    }
   ]
+}
+
+import SVGO from 'svgo'
+import path, { generatePrefix } from 'path'
+import glob from 'glob-promise'
+// const generatePrefix = file => file
+const folder = './src/assets/images/technologiesLogos/**.svg'
+const svgs = await glob(folder)
+for (let filePath of svgs) {
+  // let filePath = path.join(folder, file)
+  let prefix = path.basename(filePath)//generatePrefix(file)
+  let svgo = new SVGO({
+    plugins: [{
+      cleanupIDs: {
+        prefix: prefix
+      }
+    }]
+  })
+
+  svgo.optimize(fs.readFileSync(filePath, 'utf8'), svg => {
+    result = svg.data
+  })
 }
