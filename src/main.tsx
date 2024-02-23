0a52c3ff4badfa3fff93b1b3863aeb9e762b0386
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-02-23 22:03:50 +0400

    Add Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import '@/shared/styles/global.scss'
 import { Provider } from 'react-redux'
 import { store } from './store/index'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore typescript is not published
 import { App } from './App'
+
+import '@/shared/styles/global.scss'
+import '@/shared/styles/tailwind.css'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
