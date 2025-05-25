7c33217fd3b6b1d8fb3b6952d11d6cce2536cb6d
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-05-25 20:00:29 +0400

    Add file icon to PageWrapper's header

--- src/lib/features/DonateCurrencyCard.svelte
+++ src/lib/features/DonateCurrencyCard.svelte
@@ -27,12 +27,18 @@
   } = $props()
 
   // I thought about using interpolate-size but 1) it's only implemented in Chrome/Edge and 2) it does not work with text-overflow: ellipsis
+
+  let addressHeight: number = $state(0)
+  let clickToPayHeight: number = $state(0)
+  const expandedHeight = $derived(addressHeight + clickToPayHeight)
+  $effect(() => console.log(expandedHeight))
+  let collapseAddress = $state(true)
 </script>
 
 <SquircleThick class="bg-[#242424]/40" translucent>
   <div class="group flex h-full flex-col rounded-[30px] p-8">
     <div class="coins min-h-0 flex-1 overflow-clip">
-      <div class="flex h-[68px] duration-300">
+      <div class="flex h-[68px]">
         {@render coins()}
       </div>
     </div>
@@ -46,20 +52,36 @@
         <span class="text-[35.5px] font-bold text-white">{name}</span>
       </div>
       <div class="flex w-full items-end pt-1.5">
-        <span
-          class="interpolate-size h-[25.5px] w-full overflow-hidden font-mono text-[17px] font-semibold tracking-[-0.2px] break-all text-ellipsis text-white/60 transition-[height] select-all not-group-hover:whitespace-nowrap group-hover:h-auto"
+        <div
+          class="interpolate-size relative h-[25.5px] w-full overflow-hidden font-mono text-[17px] font-semibold tracking-[-0.2px] break-all text-ellipsis text-white/60 transition-[height] duration-300 select-all group-hover:h-[var(--address-height)]"
+          style="--address-height: {addressHeight ? `${addressHeight}px` : 'auto'}"
+          ontransitionstart={() => (collapseAddress = false)}
+          ontransitionend={() => (collapseAddress = true)}
         >
-          {address}
-        </span>
+          <span class={{ 'not-group-hover:whitespace-nowrap': collapseAddress }}>
+            {address}
+          </span>
+          <span
+            class="pointer-events-none absolute top-0 left-0 w-full opacity-0"
+            bind:offsetHeight={addressHeight}
+          >
+            {address}
+          </span>
+        </div>
       </div>
       {#if clickToPay}
-        <div class="h-0 shrink-0 overflow-clip group-hover:h-auto">
-          <div class="flex h-[50px] items-center pt-2">
+        <div
+          class="h-0 shrink-0 overflow-clip transition-[height] duration-300 group-hover:h-[58px]"
+        >
+          <div
+            class="flex h-[50px] shrink-0 items-center pt-2"
+            bind:offsetHeight={clickToPayHeight}
+          >
             {#each Object.entries(clickToPay) as [key, value] (key)}
               {@const name = key as keyof ClickToPay}
               <a
                 href={value}
-                class="flex h-full w-auto p-1 px-2"
+                class="flex h-[40px] w-auto p-1 px-2"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -83,12 +105,6 @@
 
 <style>
   .coins {
-    mask-image: linear-gradient(
-      to bottom,
-      black 0%,
-      black calc(100% - 20px),
-      transparent calc(100% - 5px),
-      transparent 100%
-    );
+    mask-image: linear-gradient(to bottom, black 0%, black calc(100% - 20px), transparent 100%);
   }
 </style>
--- src/lib/widgets/PageWrapper.svelte
+++ src/lib/widgets/PageWrapper.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { page } from '$app/state'
+
   let {
     button,
     title,
@@ -8,6 +10,10 @@
     title: string
     children?: import('svelte').Snippet
   } = $props()
+
+  // JS ONLY:
+  // There is no way to differentiate between a pressed state and drag in CSS with :active pseudoclass
+  let pressed = $state(false)
 </script>
 
 <div class="flex h-full w-full flex-col">
@@ -15,9 +21,31 @@
     class="border-px flex h-[52px] w-full shrink-0 items-center justify-between border border-x-0 border-t-0 border-white/25 px-[15px] py-[12px]"
   >
     <div class="ml-[3px] flex flex-row items-center gap-2">
-      <button></button>
-      <div class="flex flex-row items-center gap-1 text-[15px] font-semibold">
-        <span></span>
+      <!-- TODO: close button -->
+      <div class="group flex flex-row items-center gap-1 text-[15px] font-semibold">
+        <a
+          href="https://hloth.dev{page.url.pathname}"
+          aria-label="Current page"
+          class={[
+            'flex w-0 overflow-clip transition-[width] group-hover:w-5',
+            {
+              'brightness-[48%]': pressed
+            }
+          ]}
+          onpointerdown={() => (pressed = true)}
+          onpointerup={() => (pressed = false)}
+          onblur={() => (pressed = false)}
+          ondragstart={() => (pressed = false)}
+          ondragend={() => (pressed = false)}
+        >
+          <img
+            src="/file-icon.webp"
+            alt="Steve Job's quote as a text file icon"
+            width="20"
+            height="20"
+            class="h-5 min-w-5 object-left"
+          />
+        </a>
         <span class="select-none">
           {title}
         </span>
@@ -29,7 +57,7 @@
       </button>
     {/if}
   </header>
-  <div class="p-[25px] flex-1 max-h-[calc(100%-52px)]">
+  <div class="max-h-[calc(100%-52px)] flex-1 p-[25px]">
     {@render children?.()}
   </div>
 </div>
