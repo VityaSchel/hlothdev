e77144c95a9fcd1931dc94f8490288674317406c
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-02 14:07:32 +0400

    Add JavascriptDisabledMessage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,21 @@
   import Background from '$lib/widgets/Background.svelte'
   import Nav from '$lib/widgets/Nav.svelte'
   import Squircle from '$lib/Squircle.svelte'
+  import JavascriptDisabledMessage from '$lib/widgets/JavascriptDisabledMessage.svelte'
 
   let { children } = $props()
 </script>
 
 <Background>
-  <main class="relative h-[645px] w-[1384px] max-w-full drop-background-shadow rounded-[32px] shrink-0">
+  <main
+    class="drop-background-shadow relative h-[645px] w-[1384px] max-w-full shrink-0 rounded-[32px]"
+  >
     <Squircle class="bg-thick" innerStrokeWidth={2} translucent>
       {@render children()}
     </Squircle>
   </main>
   <Nav />
+  <JavascriptDisabledMessage />
 </Background>
 
 <style>
