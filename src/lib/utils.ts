3823e709b2a7f87a2e0a520e46546e74e88b5392
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-02 06:44:35 +0400

    Add slider dots to FavoriteThingsCard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
 export function isIos() {
   return /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
 }
+
+export const transition = (clampStart: number, clampEnd: number, x: number) =>
+  clampStart + (clampEnd - clampStart) * Math.max(0, Math.min(x, 1))
