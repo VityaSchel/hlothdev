14243555c34e0814d6d25c362ab7e626fde72a8f
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-09 13:25:57 +0400

    Make GitHubRepositories card scalable

--- src/routes/me/GitHubRepositoriesCard.svelte
+++ src/routes/me/GitHubRepositoriesCard.svelte
@@ -5,8 +5,8 @@
 
 <XsCard
   class="
-    col-start-1 col-end-2 row-start-11 row-end-12 flex cursor-default flex-col
-    items-start justify-between bg-gradient-to-b from-[#191919] to-[#0d0d0d] p-2
+    container-size col-start-1 col-end-2 row-start-11 row-end-12 cursor-default
+    bg-gradient-to-b from-[#191919] to-[#0d0d0d]
     px470:col-start-2 px470:col-end-3 px470:row-start-5 px470:row-end-6
     md:col-start-6 md:col-end-7 md:row-start-5 md:row-end-6
     px1180:col-start-8 px1180:col-end-9 px1180:row-start-3 px1180:row-end-4
@@ -16,11 +16,20 @@
   rel="noopener noreferrer"
   target="_blank"
 >
-  <span class="flex h-11 w-11">
-    <GithubIcon />
-  </span>
-  <div class="flex flex-col gap-0 select-none">
-    <span class="text-3xl font-medium tracking-tight text-white/80">185</span>
-    <span class="pl-1 text-xs font-semibold text-white/90">repositories</span>
+  <!-- prettier-ignore -->
+  <div class="
+    flex h-full w-full flex-col items-start justify-between p-[6.72cqw]
+  ">
+    <span class="flex h-[36.97cqw] w-[36.97cqw]">
+      <GithubIcon />
+    </span>
+    <div class="flex flex-col gap-0 select-none">
+      <span class="
+        text-[26cqw] leading-[25cqw] font-medium tracking-tight text-white/80
+      ">185</span>
+      <span class="
+        pl-[3.36cqw] text-[10.08cqw] leading-[12cqw] font-semibold text-white/90
+      ">repositories</span>
+    </div>
   </div>
 </XsCard>
