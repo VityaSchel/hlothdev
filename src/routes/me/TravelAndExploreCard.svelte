cc4404f92ef168ae42f6e53e998bc9c74a4c84f8
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-06-09 12:21:59 +0400

    Make TravelAndExplore card scalable

--- src/routes/me/TravelAndExploreCard.svelte
+++ src/routes/me/TravelAndExploreCard.svelte
@@ -5,18 +5,27 @@
 <XlCard
   bg="url(/me-in-front-of-zeda-makhuntseti-waterfall-2024.webp)"
   class="
-    one-storey-a col-start-1 col-end-4 row-start-5 row-end-8 flex flex-col gap-3
-    bg-cover px-8 py-[34px]
+    container-size one-storey-a col-start-1 col-end-4 row-start-5 row-end-8
+    bg-cover
     px470:col-start-3 px470:col-end-6 px470:row-start-4 px470:row-end-7
     md:col-start-4 md:col-end-7 md:row-start-2 md:row-end-5
   "
 >
-  <h2 class="text-[39px] leading-[39px] font-bold tracking-[0.6px]">
-    I love to travel and explore
-  </h2>
-  <p class="text-[21px] leading-[25px] font-semibold tracking-[-0.4px]">
-    Every year I try something new
-    <br />
-    I always keep up with trends
-  </p>
+  <div class="flex flex-col gap-[3.08cqw] px-[8.22cqw] py-[8.74cqw]">
+    <!-- prettier-ignore -->
+    <h2 class="
+      text-[10.02cqw] leading-[10.02cqw] font-bold tracking-[0.154cqw]
+    ">
+      I love to travel and explore
+    </h2>
+    <p
+      class="
+        text-[5.39cqw] leading-[6.42cqw] font-semibold tracking-[-0.102cqw]
+      "
+    >
+      Every year I try something new
+      <br />
+      I always keep up with trends
+    </p>
+  </div>
 </XlCard>
