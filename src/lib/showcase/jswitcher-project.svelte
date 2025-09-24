55f583d615c61a06418a2a0291410278ade87872
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-09-24 22:44:23 +0200

    Add JSwitch description

--- src/lib/showcase/jswitcher-project.svelte
+++ src/lib/showcase/jswitcher-project.svelte
@@ -12,17 +12,17 @@
 		images: [
 			{
 				...(await getShowcasePicture("jswitcher", "screenshot-1")),
-				alt: "JSwitcher screenshot 1 TODO: alt",
+				alt: "JSwitcher auth form screenshot requesting OAuth code from user",
 			},
 			{
 				...(await getShowcasePicture("jswitcher", "screenshot-2")),
-				alt: "JSwitcher screenshot 2 TODO: alt",
+				alt: "JSwitcher fonts mapping settings",
 			},
 		],
 		links: [
 			{
 				url: "https://git.hloth.dev/hloth/jswitcher",
-				label: "Open jswitcher source code",
+				label: "Open JSwitcher source code",
 			},
 		],
 	};
@@ -30,29 +30,27 @@
 
 {#snippet description()}
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Figma plugin that adds seamless component and font switching between team
+		libraries: design once, copy and switch components for any platform.
 	</p>
-	<h2>Heading</h2>
+	<h2>The task</h2>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Designer team of Russian division of the international Joom marketplace
+		contacted me to make a Figma plugin for them that would automatically
+		convert existing mobile layouts between Android and iOS platforms.
 	</p>
-	<h2>Heading</h2>
+	<h2>Challenges</h2>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		This was my first time working with Figma TypeScript SDK and it was way
+		before we had high-quality performant bundlers and DX. I had to use webpack
+		and babel. I had to learn recursive tree search to implement component
+		conversion logic. I was also required to build a OAuth client that works
+		within Figma plugin.
+	</p>
+	<h2>The final result</h2>
+	<p>
+		A few weeks later I presented the plugin that connects to the Figma team
+		library and allows user to convert layouts with a single mouse click,
+		including fonts, component instances and frame properties.
 	</p>
 {/snippet}
