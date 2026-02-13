ef04ce878f91a2ba8c1ae66fcb4ef11faabf7e46
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2026-02-13 12:58:21 +0100

    Fix error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,40 +8,42 @@
 <PageMetadata
 	name={page.status === 404 ? "Page Not Found" : `Error ${page.status}`}
 />
-<PageWrapper viewId="error">
+<PageWrapper viewId="error" class="h-full">
 	<SubpageWrapper
-		class="flex items-center justify-center"
+		class="@container-[size] flex overflow-auto"
 		title="+error.svelte"
 		srTitle="Error page"
 		backUrl="/"
 	>
-		<div
-			class="shadow-container mb-[10%] flex w-[260px] flex-col items-center
-				justify-center gap-4 rounded-[10px] bg-[#282828]/50 p-4 pt-5
-				text-center"
-		>
-			<enhanced:img
-				src="$lib/assets/error.webp?enhanced"
-				alt="Cartoon warthog head with its top blown off, revealing an exploding brain like a mushroom cloud"
-				class="text-center text-[0px]"
-				width="128"
-				height="128"
-				style="width: 64px; height: 64px"
-			/>
-			<div class="flex flex-col gap-[10px] text-white/85">
-				<h1 class="text-[13px] leading-4 font-bold">Error {page.status}</h1>
-				<p class="text-[11px] leading-[14px] font-normal">
-					{page.error?.message || "An unexpected error occurred"}
-				</p>
-			</div>
-			<a
-				href="/"
-				class="bg inset-shadow-top min-h-[28px] w-full rounded-[5px] py-[6px]
-					text-[13px] leading-4 select-none"
-				draggable="false"
+		<div class="m-auto max-w-full">
+			<div
+				class="shadow-container mb-[20cqb] flex w-[260px] max-w-full flex-col
+					items-center justify-center gap-4 rounded-[10px] bg-[#282828]/50 p-4
+					pt-5 text-center"
 			>
-				Return to Home Page
-			</a>
+				<enhanced:img
+					src="$lib/assets/error.webp?enhanced"
+					alt="Cartoon warthog head with its top blown off, revealing an exploding brain like a mushroom cloud"
+					class="text-center text-[0px]"
+					width="128"
+					height="128"
+					style="width: 64px; height: 64px"
+				/>
+				<div class="flex flex-col gap-[10px] text-white/85">
+					<h1 class="text-[13px] leading-4 font-bold">Error {page.status}</h1>
+					<p class="text-[11px] leading-[14px] font-normal">
+						{page.error?.message || "An unexpected error occurred"}
+					</p>
+				</div>
+				<a
+					href="/"
+					class="bg inset-shadow-top min-h-[28px] w-full rounded-[5px] px-2
+						py-[6px] text-[13px] leading-4 select-none"
+					draggable="false"
+				>
+					Return to Home Page
+				</a>
+			</div>
 		</div>
 	</SubpageWrapper>
 </PageWrapper>
